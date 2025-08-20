512eba98b57573013716ed187fdbfe499bad0714
Dadis <raditztv@gmail.com>
2025-08-20 14:17:12 +0200

    tp03

---
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -0,0 +1,23 @@
+package fr.diginamic.hello.controleurs;
+
+import fr.diginamic.hello.models.Ville;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/villes")
+public class VilleControleur {
+    @GetMapping
+    public List<Ville> getVilles() {
+        List<Ville> villes = new ArrayList<>();
+        villes.add(new Ville("Paris", 2000000));
+        villes.add(new Ville("Lyon", 500000));
+        villes.add(new Ville("Marseille", 800000));
+        villes.add(new Ville("Toulouse", 400000));
+        return villes;
+    }
+}
